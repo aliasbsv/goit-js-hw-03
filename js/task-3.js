4732171f852f88e6cd014584c4763a1290e8f9bc
aliasbsv <aliasbsv@gmail.com>
2024-04-13 22:44:57 +0200

    mes

--- js/task-3.js
+++ js/task-3.js
@@ -1,17 +1,16 @@
-function checkForSpam(message) { 
-  let mesNeu = message.toLowerCase();
-  if ((mesNeu.includes('spam')) || (mesNeu.includes('sale'))) {
-    return true;
+function filterArray(numbers, value) { 
+  let neuArray = [];
+  for (let i=0; i<numbers.length; i++) { 
+    if (numbers[i] > value) 
+      neuArray.push(numbers[i]);
   }
-  else { 
-    return false;
-  }
+  return neuArray;
 }
 
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing Sale, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+console.log('****** TASK 3 ****** ');
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+console.log('****** END ****** ');
